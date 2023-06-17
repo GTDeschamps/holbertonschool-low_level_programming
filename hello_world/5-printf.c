1d7a72d98d46af63177b9c4cd1f9ccc7e982c474
GTDeschamps <g.deschamps@hotmail.fr>
2023-06-17 08:11:36 -0700

    C language is a piece of art

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ *création du code en printf
+ *résultat attendu
+ *with proper grammar, but the outcome is a piece of art,
+*/
+
+init main(void)
+{
+  prinf("with proper grammar, but the outcome is a piece of art,")
+  return(0)
+ }
