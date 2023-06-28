b709780744f3e67843c0ac7a28f8ceeeb072a970
GTDeschamps <g.deschamps@hotmail.fr>
2023-06-28 02:05:47 -0700

    first array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,9 +8,15 @@
  */
 void print_array(int *a, int n)
 {
+	int i = 0;
 
-	for (n = 0; n < 5 ; n++)
+	for (i = 0; i < n ; i++)
 	{
-		printf("[%d]", a[n]);
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
 	}
+	printf("\n");
 }
