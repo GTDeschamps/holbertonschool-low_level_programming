1ebda07111bdfd0bcb44016e7322e1e0f67e40f3
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-03 07:38:11 -0700

    prime number

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ *is_prime_number - Entry point
+ *@n: ask number
+ *Return: integer
+ */
+
+int is_prime_number(int n)
+{
+	int i;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+
+	return (1);
+}
