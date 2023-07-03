f02d9b9e646f66430c73081d2d636d67d19adde7
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-03 02:05:02 -0700

    reverse recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ *_print_rev_recursion - Entry point
+ *@s: string
+ *Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
