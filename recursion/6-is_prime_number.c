d5ef3c582391081f53518ad3f98c760e5947163b
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-04 07:46:03 -0700

    prime number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,21 +1,33 @@
 #include"main.h"
 /**
  *is_prime_number - Entry point
- *@n: ask number
+ *is_divisible - Recursive function
+ *@divisor: ask number
+ *@n: prime number
  *Return: integer
  */
 
-int divisor(int i)
+int is_divisible(int n, int divisor)
 {
-	if (i == 1)
+	if (divisor == 1)
+	{
 		return (1);
-}
-
-int is_prime_number(int n)
-{
-	if (n % divisor == 0 || n <= 1 )
+	}
+	if (n % divisor == 0)
 	{
 		return (0);
 	}
-	return is_prime_number(n, n - 1);
+	{
+		return (is_divisible(n, divisor - 1));
+	}
+}
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	{
+		return (is_divisible(n, n - 1));
+	}
 }
