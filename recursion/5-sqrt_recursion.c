12dcb1e449d6da8fb7ea16521d9ff43db6981b89
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-04 07:26:51 -0700

    square root

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include"main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n : variable
+ * @x : variable
+ * Return: void
+ */
+
+int _sqrt_helper(int n, int x);
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_helper(n, 1));
+}
+
+/**
+ * _sqrt_helper - avoid overflow
+ * @n : variable
+ * @x : variable
+ * Return: void
+ */
+
+int _sqrt_helper(int n, int x)
+{
+	float q;
+
+	if (n <= 0)
+		return (-1);
+	if (x * x > n)
+		return (-1);
+	q = (float)n / x;
+	if (q == x)
+		return ((int)q);
+	return (_sqrt_helper(n, x + 1));
+}
--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -5,22 +5,17 @@
  *Return: integer
  */
 
+int divisor(int i)
+{
+	if (i == 1)
+		return (1);
+}
+
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
+	if (n % divisor == 0 || n <= 1 )
 	{
 		return (0);
 	}
-
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-
-	return (1);
+	return is_prime_number(n, n - 1);
 }
