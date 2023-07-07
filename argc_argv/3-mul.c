775ae0c29bc17b25076dc080cddbdfbeb156c680
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-07 06:02:52 -0700

    multiplier

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,7 +14,8 @@ int main(int argc, char *argv[])
 
 	if (argc < 3)
 	{
-		printf("error \n");
+		printf("Error\n");
+		return (1);
 	}
 	result = (atoi(argv[1]) * atoi(argv[2]));
 	printf("%d\n", result);
