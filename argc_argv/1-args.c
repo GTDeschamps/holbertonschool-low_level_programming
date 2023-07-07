2355a53fcfda7304a6448a6f0cd1e0624d5c0624
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-07 01:43:46 -0700

    numbers of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ *main - Entry point
+ *@argc: argument
+ *@argv: value
+ *
+ *Return:void
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+
+	return (0);
+}
