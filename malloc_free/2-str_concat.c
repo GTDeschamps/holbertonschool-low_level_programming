2d5d94f8f756e8cf6122f1af123ea2905761a928
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-10 07:54:43 -0700

    concat with malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -22,10 +22,6 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 	concat = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (concat == NULL)
-	{
-		return (NULL);
-	}
 	strcpy(concat, s1);
 	strcat(concat, s2);
 	return (concat);
