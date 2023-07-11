16b3fc95be845520c5c6c328c1f3eee5fb2551b2
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-10 23:31:54 -0700

    concat with malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -4,10 +4,9 @@
 #include <string.h>
 
 /**
- *str_concat: Entry point
+ *str_concat - Entry point
  *@s1: string 1
  *@s2: string 2
- *
  *Return: concat
  */
 
@@ -17,11 +16,15 @@ char *str_concat(char *s1, char *s2)
 	size_t len1 = strlen(s1);
 	size_t len2 = strlen(s2);
 
-	if ((s1 == NULL) || (s2 == NULL))
+	if ((s1 == NULL) && (s2 == NULL))
 	{
 		return (NULL);
 	}
 	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
 	strcpy(concat, s1);
 	strcat(concat, s2);
 	return (concat);
