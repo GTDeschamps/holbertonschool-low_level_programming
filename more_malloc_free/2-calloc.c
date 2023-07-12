f9472f32eafee295c12869d5a79c19484f458a22
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-12 07:15:29 -0700

    nconcat

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include<stdlib.h>
+/**
+ *calloc - Entry point
+ *@nmemb: number of memory
+ *@size: size allocated
+ *Return: function alloc
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
