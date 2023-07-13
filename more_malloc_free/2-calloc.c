313a730d608e2748c3a069607c8d87430c660650
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-13 01:03:51 -0700

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  *calloc - Entry point
  *@nmemb: number of memory
@@ -9,3 +10,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int totalsize;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	totalsize = (nmemb * size);
+	ptr = malloc(totalsize);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	memset(ptr, 0, totalsize);
+	return (ptr);
+}
