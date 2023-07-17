fbe0cff0a945ea0c51ca4eba1dbf17fc75d39cfb
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-17 05:03:28 -0700

    first structure

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *init_dog -Entry point
+ *@d: value of structure
+ *@name: name of the dog
+ *@age: age of this
+ *@owner: who is the master
+ *Return: void
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
