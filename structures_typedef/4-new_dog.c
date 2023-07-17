83a85a593c7bfa11b99fb6883d406c0ffdbf39ef
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-17 07:40:44 -0700

    a new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include <string.h>
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *new_dog - entry point
+ *@name: name of new dog
+ *@age: how old
+ *@owner: another master
+ *Return: dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (newDog == NULL)
+	{
+		return (NULL);
+	}
+
+	newDog->name = (char *)malloc(strlen(name) + 1);
+	newDog->owner = (char *)malloc(strlen(owner) + 1);
+	if (newDog->name == NULL || newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog->owner);
+		free(newDog);
+		return (NULL);
+	}
+	strcpy(newDog->name, name);
+	strcpy(newDog->owner, owner);
+
+	newDog->age = age;
+
+	return (newDog);
+}
