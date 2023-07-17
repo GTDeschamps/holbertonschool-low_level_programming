1881ecb52585039795c09380d9ccd44647d69f37
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-17 02:02:22 -0700

    creation of first structure call dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #include <stdio.h>
 /**
- *struct dog: structure of variable who define the dog
+ *struct dog - Entry point
+ *@name: variable define the name of the dog
+ *@age: variable define this age
+ *@owner: variable define the owner
  */
 
 struct dog
