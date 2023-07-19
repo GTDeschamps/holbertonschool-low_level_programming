6f15f1340e6ae9a74ee61d11c03e348084dea71b
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-19 10:44:18 +0200

    2 task  in one push

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stddef.h>
 
 /**
  *array_iterator - Entry point
@@ -15,7 +15,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i=0; i < size; i++)
+	if (i == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < size; i++)
 	{
 		(*action)(array[i]);
 	}
