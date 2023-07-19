91eb4f12b47f14433d918b4adfc05dfd9ca88a60
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-19 10:34:44 +0200

    number of element in array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ *array_iterator - Entry point
+ *@array: array zone
+ *@size: size of element
+ *@action: function pointer
+ *Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i=0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * int_index -Entry point
+ * @array: array of element
+ * @size: number of element
+ * @cmp: pointer to function
+ * Return: integer
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	
+	if (size <= 0)
+	{
+		return -1;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
