c345073358ffacd6dc6632baa07ab3135ed15f8d
GTDeschamps <g.deschamps@hotmail.fr>
2023-07-20 14:16:09 +0200

    correction task 2 project 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -14,6 +14,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	if (cmp == NULL)
+	{
+		return (-1);
+	}
+
 	if (size <= 0)
 	{
 		return (-1);
