e315d1a2e312d4571998e9f7e678e8b8761ce7c2
GTDeschamps <g.deschamps@hotmail.fr>
2023-08-01 14:35:22 +0200

    task 4 project 1: free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - Entry point
+ * Description:  add in the list
+ * @head: start the list
+ * @str: element of list
+ * Return: void
+*/
+
+void free_list(list_t *head)
+{
+	list_t *current;
+	list_t *next;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
