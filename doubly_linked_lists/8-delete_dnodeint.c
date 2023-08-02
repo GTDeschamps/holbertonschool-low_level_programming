6db6ac78ea05aca07433c723fb75911926283d31
GTDeschamps <g.deschamps@hotmail.fr>
2023-08-02 19:51:23 +0200

    project 2 task 8: delete index

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Entry point
+ * Description:  add in the list
+ * @head:list
+ * @index: index
+ * Return: list
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *head;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = current->next;
+		if (current->next != NULL)
+		{
+			current->next->prev = current->prev;
+		}
+		free(current);
+		return (1);
+	}
+
+	while (current != NULL && count < index)
+	{
+		current = current->next;
+		count++;
+	}
+	if (current == NULL)
+		return (-1);
+
+	current->prev->next = current->next;
+	if (current->next != NULL)
+	{
+		current->next->prev = current->prev;
+	}
+	free(current);
+	return (1);
+}
