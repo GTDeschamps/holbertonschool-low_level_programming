f1eb8fa82d6d5164abb209f5ed6709ed98e9ba04
GTDeschamps <g.deschamps@hotmail.fr>
2023-08-07 14:27:52 +0200

    correction main.h

--- file_io/main.h
+++ file_io/main.h
@@ -3,8 +3,7 @@
 
 #include <fcntl.h>
 #include <stdio.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
