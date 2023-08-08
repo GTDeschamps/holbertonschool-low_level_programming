ffd0033fbe9b550faef80ac6986e0b4b29324b90
GTDeschamps <g.deschamps@hotmail.fr>
2023-08-08 10:16:38 +0200

    Project 1 Task 1: create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
 *create_file - Entry point
@@ -12,10 +13,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int descript;
+	ssize_t bytes_wrt;
 
 	if (filename == NULL)
 		return (-1);
+
+
+	descript = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (descript == -1)
+		return (-1);
+
 	if (text_content == NULL)
-		return;
-		
+	{
+		bytes_wrt = write(descript, text_content, strlen(text_content));
+		if (bytes_wrt == -1)
+		{
+			close(descript);
+			return (-1);
+		}
+	}
+	close(descript);
+	return (1);
 }
