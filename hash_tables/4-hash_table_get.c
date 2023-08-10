5f19590f9895ab0fbba626480e2e0453e739c2f1
GTDeschamps <g.deschamps@hotmail.fr>
2023-08-10 19:20:08 +0200

    project 2, task 4: hash table get

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -4,14 +4,33 @@
 #include "hash_tables.h"
 
 /**
- *hash_table_set - Entry point
+ *hash_table_get - Entry point
  *@ht: pointer to table
  *@key: character
- *@value:  value of index
- *Return: integer
+ *Return: character
 */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	if (temp == NULL)
+		return (NULL);
+
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		return (temp->value);
+
+		temp = temp->next;
+		}
+
+	return (NULL);
 }
