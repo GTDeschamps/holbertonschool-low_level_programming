1c0a3dd0431b7d9135a922192da0592749321702
GTDeschamps <g.deschamps@hotmail.fr>
2023-08-11 14:04:59 +0200

    fizz-buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,39 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-/**
- *main -Entry point
- *Return: always 0
- */
-
-int main(void)
+int main (void)
 {
-	int i;
+    int i;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%d", i);
-		}
-
-		if (i != 100)
-		{
-			printf(" ");
-		}
-	}
-	printf("\n");
-	return (0);
+    for (i = 0; i < 100; i++)
+    {
+       if (i % 3 == 0 && i % 5 == 0)
+       {
+           printf("FizzBuzz");
+       }
+       else if (i % 3 == 0)
+       {
+           printf("Fizz");
+       }
+       else if (i % 5 == 0)
+       {
+           printf("Buzz");
+       }
+       else
+       {
+           printf("%d",i);
+       }
+       if (i != 100)
+       {
+           printf(" ");
+       }
+    }
+    printf("\n");
+    return (0);
 }
