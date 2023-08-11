6d49f5a29c554566df9c1cbc3c0041f0c60a1c15
GTDeschamps <g.deschamps@hotmail.fr>
2023-08-11 11:30:14 +0200

    correction of task 6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -11,8 +11,27 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *node;
+	unsigned long int i;
+	hash_node_t *temp;
+
 	if (ht == NULL)
 		return;
 
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+
+		while (node != NULL)
+		{
+			temp = node;
+			node = node->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+
+	free(ht->array);
 	free(ht);
 }
